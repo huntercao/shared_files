cfd10a3c74c4db3e6b1a64db535d115cf17f194b
Hunter Cao <yudi.cao@gmail.com>
2018-10-06 11:51:33 +0800

    Add the new mid files.

--- midi/src/java/MidiFile/src/scale_notes/midifile/MidiFile.java
+++ midi/src/java/MidiFile/src/scale_notes/midifile/MidiFile.java
@@ -237,26 +237,11 @@ public class MidiFile {
   {
     MidiFile mf = new MidiFile();
 
-    // Test 1 — play a C major chord
-
-    // Turn on all three notes at start-of-track (delta=0) 
-    mf.noteOn (0, 60, 127);
-    mf.noteOn (0, 64, 127);
-    mf.noteOn (0, 67, 127);
-
-    // Turn off all three notes after one minim. 
-    // NOTE delta value is cumulative — only _one_ of
-    //  these note-offs has a non-zero delta. The second and
-    //  third events are relative to the first
-    mf.noteOff (MINIM, 60);
-    mf.noteOff (0, 64);
-    mf.noteOff (0, 67);
-
     // Test 2 — play a scale using noteOnOffNow
     //  We don't need any delta values here, so long as one
     //  note comes straight after the previous one 
 
-    mf.noteOnOffNow (QUAVER, 60, 127);
+    mf.noteOnOffNow (0, 60, 127);
     mf.noteOnOffNow (QUAVER, 62, 127);
     mf.noteOnOffNow (QUAVER, 64, 127);
     mf.noteOnOffNow (QUAVER, 65, 127);
@@ -265,30 +250,18 @@ public class MidiFile {
     mf.noteOnOffNow (QUAVER, 71, 127);
     mf.noteOnOffNow (QUAVER, 72, 127);
 
-    // Test 3 — play a short tune using noteSequenceFixedVelocity
-    //  Note the rest inserted with a note value of -1
 
-    int[] sequence = new int[]
-      {
-      60, QUAVER + SEMIQUAVER,
-      65, SEMIQUAVER,
-      70, CROTCHET + QUAVER,
-      69, QUAVER,
-      65, QUAVER / 3,
-      62, QUAVER / 3,
-      67, QUAVER / 3,
-      72, MINIM + QUAVER,
-      -1, SEMIQUAVER,
-      72, SEMIQUAVER,
-      76, MINIM,
-      };
+    mf.noteOnOffNow (QUAVER, 72, 127);
+    mf.noteOnOffNow (QUAVER, 71, 127);
+    mf.noteOnOffNow (QUAVER, 69, 127);
+    mf.noteOnOffNow (QUAVER, 67, 127);
+    mf.noteOnOffNow (QUAVER, 65, 127);
+    mf.noteOnOffNow (QUAVER, 64, 127);
+    mf.noteOnOffNow (QUAVER, 62, 127);
+    mf.noteOnOffNow (QUAVER, 60, 127);
 
-    // What the heck — use a different instrument for a change
-    mf.progChange (10);
+    mf.writeToFile ("scale_c4.mid");
 
-    mf.noteSequenceFixedVelocity (sequence, 127);
-
-    mf.writeToFile ("test1.mid");
   }
     
 }
--- midi/src/java/MidiFile/src/sequnece_instrument_notes/midifile/MidiFile.java
+++ midi/src/java/MidiFile/src/sequnece_instrument_notes/midifile/MidiFile.java
@@ -237,34 +237,6 @@ public class MidiFile {
   {
     MidiFile mf = new MidiFile();
 
-    // Test 1 — play a C major chord
-
-    // Turn on all three notes at start-of-track (delta=0) 
-    mf.noteOn (0, 60, 127);
-    mf.noteOn (0, 64, 127);
-    mf.noteOn (0, 67, 127);
-
-    // Turn off all three notes after one minim. 
-    // NOTE delta value is cumulative — only _one_ of
-    //  these note-offs has a non-zero delta. The second and
-    //  third events are relative to the first
-    mf.noteOff (MINIM, 60);
-    mf.noteOff (0, 64);
-    mf.noteOff (0, 67);
-
-    // Test 2 — play a scale using noteOnOffNow
-    //  We don't need any delta values here, so long as one
-    //  note comes straight after the previous one 
-
-    mf.noteOnOffNow (QUAVER, 60, 127);
-    mf.noteOnOffNow (QUAVER, 62, 127);
-    mf.noteOnOffNow (QUAVER, 64, 127);
-    mf.noteOnOffNow (QUAVER, 65, 127);
-    mf.noteOnOffNow (QUAVER, 67, 127);
-    mf.noteOnOffNow (QUAVER, 69, 127);
-    mf.noteOnOffNow (QUAVER, 71, 127);
-    mf.noteOnOffNow (QUAVER, 72, 127);
-
     // Test 3 — play a short tune using noteSequenceFixedVelocity
     //  Note the rest inserted with a note value of -1
 
@@ -284,11 +256,23 @@ public class MidiFile {
       };
 
     // What the heck — use a different instrument for a change
+
+    mf.noteSequenceFixedVelocity (sequence, 127);
+
     mf.progChange (10);
 
     mf.noteSequenceFixedVelocity (sequence, 127);
 
-    mf.writeToFile ("test1.mid");
+    mf.progChange (6);
+
+    mf.noteSequenceFixedVelocity (sequence, 127);
+
+    mf.progChange (25);
+
+    mf.noteSequenceFixedVelocity (sequence, 127);
+
+    mf.writeToFile ("instrument_sequences.mid");
+
   }
     
 }
